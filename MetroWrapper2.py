1c646fa0d14ad453194fc711ec1f382386f1ab99
Joseph Haaga <haaga.joe@gmail.com>
2016-05-22 23:08:42 -0400

    JSON objects

--- MetroWrapper2.py
+++ MetroWrapper2.py
@@ -1,6 +1,6 @@
-
+from firebase import firebase;
 from bs4 import BeautifulSoup;
-
+import sys;
 import urllib2;
 
 url = 'http://www.wmata.com/rider_tools/pids/showpid.cfm?station_id=42';
@@ -11,21 +11,21 @@ soup = BeautifulSoup(page.read());
 
 
 tables = soup.find_all("tbody");
-
+print "Metro train arrivals";
+trains = [];
 for table in tables:
 	for row in table.find_all("tr"):
 		cellsInCurrentRow = row.find_all('td');
 		try:
 			line_color=cellsInCurrentRow[0].img.get("alt");
-			print line_color;
 			number_of_cars = cellsInCurrentRow[1].string;
-			print number_of_cars;
 			destination = cellsInCurrentRow[2].string;
-			print destination;
-
 			minutes_to_arrival = int(cellsInCurrentRow[3].string);
-			print minutes_to_arrival;
-			print;
+##			print;
+			trains.append({'line':line_color,'number_of_cars':number_of_cars,'destination':destination,'minutes_to_arrival':minutes_to_arrival});
 		except:
-			print "Skipped empty row";
-			print;
+##			print "Skipped empty row";
+			# print;
+
+for train in trains:
+	print train;
